07ccd1d79e5201f462c450f48503f2047991a56a
@Ibroximjon2003 <maxammadjonovibroximjon24@gmail.com>
2023-04-08 15:09:14 +0500

    cubit

---
+++ lib/constants/constants.dart
@@ -0,0 +1,23 @@
+
+
+const names = [
+  'boy',
+  'girl',
+  'father',
+  'mother',
+  'brother',
+  'sister',
+  'hotel',
+  'hospital',
+  'school',
+  'office',
+  'stadium',
+  'dentist',
+  'doctor',
+  'teacher',
+  'professor',
+  'gardener',
+  'master',
+  'soldier',
+  'car',
+];
